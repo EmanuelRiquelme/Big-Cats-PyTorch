8c9704355ce3d462ed350d0f38b66a3c0588cb29
EmanuelRiquelme <112744690+EmanuelRiquelme@users.noreply.github.com>
2023-05-05 02:09:06 -0400

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -4,7 +4,7 @@ import os
 from  PIL import Image
 from torchvision import transforms
 
-class Big-Cats(Dataset):
+class Big_Cats(Dataset):
     def __init__(self, root_dir = 'animals',transform = False):
         self.root_dir = root_dir
         self.labels = self.__getlabels__()
